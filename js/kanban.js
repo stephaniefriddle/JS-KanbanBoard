fb13dd7f202d2fe627ef44ce22db0e653ea80c9d
Stephanie Friddle <stephanie.friddle@gmail.com>
2021-11-09 14:44:13 -0500

    Step 6

--- js/kanban.js
+++ js/kanban.js
@@ -31,6 +31,9 @@ const create_item = () => {
 
     let input = document.createElement('input');
     item.appendChild(input);
+
+    let save_btn = document.createElement('button');
+    save_btn.innerHTML = "Save";
 };
 
 document.querySelectorAll('.drop').forEach(element => {
